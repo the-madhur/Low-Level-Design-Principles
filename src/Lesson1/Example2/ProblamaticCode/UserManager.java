f72ab8e0e84109edc75adc0a64ef386442e47b59
the-madhur <madhurhp245@gmail.com>
2025-03-06 17:07:20 +0530

    Problamatic commit

---
+++ src/Lesson1/Example2/ProblamaticCode/UserManager.java
@@ -0,0 +1,19 @@
+package Lesson1.Example2.ProblamaticCode;
+
+public class UserManager 
+{
+    public void createUser()
+    {
+        // create a user
+    }
+
+    public void deleteUser()
+    {
+        // delete a user
+    }
+
+    public void ActivityLogger()
+    {
+        // logging activity of user
+    }
+}
