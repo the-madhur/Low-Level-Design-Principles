20c76387dfcdc40b6e5659f13e7c53f727651b9d
the-madhur <madhurhp245@gmail.com>
2025-03-06 16:09:31 +0530

    employee commit

---
+++ src/Lesson1/Example1/BetterCode/Employee.java
@@ -0,0 +1,16 @@
+package Lesson1.Example1.BetterCode;
+
+public class Employee 
+{
+    private final int employeeId;
+
+    public Employee(int id)
+    {
+        this.employeeId = id;
+    }
+
+    public int getId()
+    {
+        return this.employeeId;
+    }
+}
