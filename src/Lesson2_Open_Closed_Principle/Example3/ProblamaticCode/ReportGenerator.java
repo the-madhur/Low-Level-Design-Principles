0ffe5ff3ac5f5c0399957a4940644a4a38a0eb59
the-madhur <madhurhp245@gmail.com>
2025-03-07 16:43:02 +0530

    Exp 3 Problamatic

---
+++ src/Lesson2_Open_Closed_Principle/Example3/ProblamaticCode/ReportGenerator.java
@@ -0,0 +1,19 @@
+package Lesson2_Open_Closed_Principle.Example3.ProblamaticCode;
+
+public class ReportGenerator 
+{
+    public String generateReportType(String reportType)
+    {
+        if(reportType.equals("PDF"))
+        {
+            return "Genertaing PDF Report";
+        }
+        else if(reportType.equals("WORD"))
+        {
+            return "Generating Word Report";
+        }
+        
+        return "Report type not supported";
+    }
+
+}
