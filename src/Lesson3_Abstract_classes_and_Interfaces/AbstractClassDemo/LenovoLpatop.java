3d351ba92e16450fb4cf6b0992449a62b478bd40
the-madhur <madhurhp245@gmail.com>
2025-03-08 17:51:47 +0530

    lenovo

---
+++ src/Lesson3_Abstract_classes_and_Interfaces/AbstractClassDemo/LenovoLpatop.java
@@ -0,0 +1,14 @@
+package Lesson3_Abstract_classes_and_Interfaces.AbstractClassDemo;
+
+public class LenovoLpatop extends Product
+{
+    public double calculateDiscount()
+    {
+        return 0;
+    }
+
+    public void termAndConditions()
+    {
+        
+    }
+}
