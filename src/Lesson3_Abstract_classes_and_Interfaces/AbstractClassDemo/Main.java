cac21d127702bfd43cd1805a2c075528d9b3808c
the-madhur <madhurhp245@gmail.com>
2025-03-08 17:45:26 +0530

    main

---
+++ src/Lesson3_Abstract_classes_and_Interfaces/AbstractClassDemo/Main.java
@@ -0,0 +1,10 @@
+package Lesson3_Abstract_classes_and_Interfaces.AbstractClassDemo;
+
+public class Main 
+{
+    public static void main(String[] args) {
+        Product p = new Macbook();
+        p.termAndConditions();
+    }
+
+}
