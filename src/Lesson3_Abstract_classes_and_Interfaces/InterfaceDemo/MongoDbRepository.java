8c74a255da1ce8155285d8648b6228deff49e6e3
the-madhur <madhurhp245@gmail.com>
2025-03-08 21:48:41 +0530

    interface Demo

---
+++ src/Lesson3_Abstract_classes_and_Interfaces/InterfaceDemo/MongoDbRepository.java
@@ -0,0 +1,10 @@
+package Lesson3_Abstract_classes_and_Interfaces.InterfaceDemo;
+
+public class MongoDbRepository 
+{
+    getUser()
+    {
+        System.out.println("User from MongoDb ");
+    }
+
+}
