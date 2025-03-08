2f8d133432fcd2d1b222ae7b1e5c3e9de5688893
the-madhur <madhurhp245@gmail.com>
2025-03-08 17:52:11 +0530

    updated main

--- src/Lesson3_Abstract_classes_and_Interfaces/AbstractClassDemo/Main.java
+++ src/Lesson3_Abstract_classes_and_Interfaces/AbstractClassDemo/Main.java
@@ -4,7 +4,10 @@ public class Main
 {
     public static void main(String[] args) {
         Product p = new Macbook();
-        p.termAndConditions();
+        p.termsAndConditions();
+
+        Product ll = new LenovoLpatop();
+        ll.termsAndConditions();
     }
 
 }
