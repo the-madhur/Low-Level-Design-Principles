7f0e47bb261af3d3f45c654a35e5bfb4a8c7491f
the-madhur <madhurhp245@gmail.com>
2025-03-08 17:45:40 +0530

    Macbook

---
+++ src/Lesson3_Abstract_classes_and_Interfaces/AbstractClassDemo/Macbook.java
@@ -0,0 +1,15 @@
+package Lesson3_Abstract_classes_and_Interfaces.AbstractClassDemo;
+
+public class Macbook extends Product
+{
+    public double calculateDiscount()
+    {
+        return 0;
+    }
+
+    public void termAndConditions()
+    {
+        System.out.println("Some Terms");
+    }
+
+}
