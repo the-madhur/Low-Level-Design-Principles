b8364f7eef088be593800ba8979e7262e0217fa5
the-madhur <madhurhp245@gmail.com>
2025-03-08 17:11:57 +0530

    Product

---
+++ src/Lesson3_Abstract_classes_and_Interfaces/AbstractClassDemo/Product.java
@@ -0,0 +1,11 @@
+package Lesson3_Abstract_classes_and_Interfaces.AbstractClassDemo;
+
+public abstract class Product 
+{
+    public abstract double calculateDiscount();
+
+    public void termAndConditions()
+    {
+        System.out.println("Macbook Terms and Conditions");
+    }
+}
