b1820009608c6375c22a60bc6d126748678ed61d
the-madhur <madhurhp245@gmail.com>
2025-03-09 10:06:44 +0530

    VISA

---
+++ src/Lesson4_Liskov_Substitution_Principle/Example1/ProblematicCode/VISACard.java
@@ -0,0 +1,31 @@
+package Lesson4_Liskov_Substitution_Principle.Example1.ProblematicCode;
+
+public class VISACard extends CreditCard
+{
+
+    @Override
+    public void tapAndPay() 
+    {
+        System.out.println("Tap and Pay impl of VISA");
+    }
+
+    @Override
+    public void onlineTransfer() 
+    {
+        System.out.println("Online Transfer impl of VISA");
+    }
+
+    @Override
+    public void swipeAndPay() 
+    {
+        System.out.println("Swipe and Pay impl of VISA");
+    }
+
+    @Override
+    public void mandatePayments() 
+    {
+        System.out.println("Mandate Payments impl of VISA");
+    }
+    
+
+}
