9be19528c4f74d3d15d2bb4d06e44cd04f9728de
the-madhur <madhurhp245@gmail.com>
2025-03-09 14:15:48 +0530

    Better Code

---
+++ src/Lesson4_Liskov_Substitution_Principle/Example1/BetterCode/CreditCard.java
@@ -0,0 +1,36 @@
+package Lesson4_Liskov_Substitution_Principle.Example1.BetterCode;
+
+public abstract class CreditCard 
+{
+    private String ownerName;
+    private String ccNumber;
+    private int cvv;
+    
+    public void setCcNumber(String ccNumber)
+    {
+        this.ccNumber = ccNumber;
+    }
+
+    public void setCVV(int cvv)
+    {
+        this.cvv = cvv;
+    }
+    public void setOwnerName(String ownerName)
+    {
+        this.ownerName = ownerName;
+    }
+    
+    public abstract void tapAndPay();
+
+    public abstract void onlineTransfer();
+
+    public abstract void swipeAndPay();
+
+    public abstract void mandatePayments();
+
+    public void displayCreditCardDetails()
+    {
+        System.out.println("CC Number: "+this.ccNumber+" , With Owner Name: "+this.ownerName);
+    }
+
+}
