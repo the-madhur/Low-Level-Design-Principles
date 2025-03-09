e3323feb692589e063b7667341e6a79c81c23c86
the-madhur <madhurhp245@gmail.com>
2025-03-09 10:06:56 +0530

    MasterCard

---
+++ src/Lesson4_Liskov_Substitution_Principle/Example1/ProblematicCode/MasterCard.java
@@ -0,0 +1,30 @@
+package Lesson4_Liskov_Substitution_Principle.Example1.ProblematicCode;
+
+public class MasterCard extends CreditCard
+{
+
+    @Override
+    public void tapAndPay() 
+    {
+        System.out.println("Tap and Pay impl of MasterCard");
+    }
+
+    @Override
+    public void onlineTransfer() 
+    {
+        System.out.println("Online Transfer impl of MasterCard");
+    }
+
+    @Override
+    public void swipeAndPay() 
+    {
+        System.out.println("Swipe and Pay impl of MasterCard");
+    }
+
+    @Override
+    public void mandatePayments() 
+    {
+        System.out.println("Mandate Payments impl of MasterCard");
+    }
+    
+}
