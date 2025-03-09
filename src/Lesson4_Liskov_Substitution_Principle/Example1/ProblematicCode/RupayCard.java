3adcc5ad23e20006d388104e54a41f5fa886d8e2
the-madhur <madhurhp245@gmail.com>
2025-03-09 10:07:04 +0530

    Rupay

---
+++ src/Lesson4_Liskov_Substitution_Principle/Example1/ProblematicCode/RupayCard.java
@@ -0,0 +1,31 @@
+package Lesson4_Liskov_Substitution_Principle.Example1.ProblematicCode;
+
+public class RupayCard extends CreditCard
+{
+
+    @Override
+    public void tapAndPay() 
+    {
+        System.out.println("Tap and Pay impl of Rupay");
+    }
+
+    @Override
+    public void onlineTransfer() 
+    {
+        System.out.println("Online Transfer impl of Rupay");
+    }
+
+    @Override
+    public void swipeAndPay() 
+    {
+        System.out.println("Swipe and Pay impl of Rupay");
+    }
+
+    @Override
+    public void mandatePayments() 
+    {
+        System.out.println("Mandate Payments impl of Rupay");
+    }
+    
+}
+
