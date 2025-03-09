0a093ed3ff0028e4d794a6cea77411630b2ffbd8
the-madhur <madhurhp245@gmail.com>
2025-03-09 14:34:03 +0530

    main

---
+++ src/Lesson4_Liskov_Substitution_Principle/Example1/ProblematicCode/Main.java
@@ -0,0 +1,21 @@
+package Lesson4_Liskov_Substitution_Principle.Example1.ProblematicCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        List<CreditCard> cards = new ArrayList<>();
+        for(CreditCard card : cards)
+        {
+            if(card instanceof RupayCard)
+            {
+                card.upiPayments();
+            }
+        }
+        
+    }
+
+}
