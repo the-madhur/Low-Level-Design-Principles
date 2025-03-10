59d1afb2e93307d5eb2ee53423916137eb3bdc9e
the-madhur <madhurhp245@gmail.com>
2025-03-10 13:54:51 +0530

    Better Code

---
+++ src/Lesson5_Interface_Segregation_Principle/Example1/BetterCode/Seller.java
@@ -0,0 +1,25 @@
+package Lesson5_Interface_Segregation_Principle.Example1.BetterCode;
+
+public class Seller implements ICanBuy, ICanModify, ICanSell
+{
+
+    @Override
+    public void buy() 
+    {
+        
+    }
+
+    @Override
+
+    public void modify() 
+    {
+        
+    }
+
+    @Override
+    public void sell() 
+    {
+        
+    }
+
+}
---
+++ src/Lesson5_Interface_Segregation_Principle/Example1/BetterCode/User.java
@@ -0,0 +1,10 @@
+package Lesson5_Interface_Segregation_Principle.Example1.BetterCode;
+
+public class User implements ICanBuy
+{
+    @Override
+    public void buy()
+    {
+        
+    }
+}
