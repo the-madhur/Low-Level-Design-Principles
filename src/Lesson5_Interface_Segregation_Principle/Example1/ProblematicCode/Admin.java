e355fcd3f99148afc3ee04e316c8ab4805238ec4
the-madhur <madhurhp245@gmail.com>
2025-03-10 11:13:37 +0530

    admin

---
+++ src/Lesson5_Interface_Segregation_Principle/Example1/ProblematicCode/Admin.java
@@ -0,0 +1,31 @@
+package Lesson5_Interface_Segregation_Principle.Example1.ProblematicCode;
+
+public class Admin implements User 
+{
+
+    @Override
+    public boolean canBuyProduct() 
+    {
+       return false;
+    }
+
+    @Override
+    public boolean canAddProduct() 
+    {
+        return false;
+    }
+
+    @Override
+    public boolean canModifyProduct() 
+    {
+        return false;
+    }
+
+    @Override
+    public boolean canApproveProduct() 
+    {
+        return false;
+    }
+
+
+}
