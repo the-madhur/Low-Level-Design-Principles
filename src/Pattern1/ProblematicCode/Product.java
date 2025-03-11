128a78a6181508d373b3b65be02b0b4b3e4d250d
the-madhur <madhurhp245@gmail.com>
2025-03-11 15:17:43 +0530

    Builder Pattern

---
+++ src/Pattern1/ProblematicCode/Product.java
@@ -0,0 +1,38 @@
+package Pattern1.ProblematicCode;
+
+import java.util.List;
+
+public class Product 
+{
+    private String name;
+
+    private String desc;
+
+    private int price;
+
+    private String brand;
+    
+    private String category;
+
+    private int discount;
+
+    private String createdAt;
+
+    private String updatedAt;
+
+    private List<String> images;
+
+    Product(String name, String desc, int price, String brand, String category, int discount, String createdAt, String upadatedAt, List<String> images)
+    {
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+        this.brand = brand;
+        this.category = category;
+        this.discount = discount;
+        this.createdAt = createdAt;
+        this.updatedAt = upadatedAt;
+        this.images = images;
+    }
+
+}
