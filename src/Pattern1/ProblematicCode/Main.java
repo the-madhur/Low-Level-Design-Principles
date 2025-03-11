2642647d51776433e5e2a5f44530637eb21b1acd
the-madhur <madhurhp245@gmail.com>
2025-03-11 15:17:39 +0530

    Builder Pattern

---
+++ src/Pattern1/ProblematicCode/Main.java
@@ -0,0 +1,12 @@
+package Pattern1.ProblematicCode;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Product p = new Product(null, null, 0, null, null, 0, null, null, null);
+
+        
+    }
+
+}
