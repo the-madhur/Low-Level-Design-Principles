db67938dd842babef42be93b40b95246264f3bfd
the-madhur <madhurhp245@gmail.com>
2025-03-11 15:17:26 +0530

    Builder Pattern

---
+++ src/Pattern1/BetterCode/v2/Product.java
@@ -0,0 +1,35 @@
+package Pattern1.BetterCode.v2;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Product 
+{
+    private String name;
+
+    private String desc;
+
+    private int price;
+
+    private String brand;
+    
+    private String category;
+
+    private int discount;
+
+    private String createdAt;
+
+    private String updatedAt;
+
+    private List<String> images;
+
+    // default Constructor
+    Product(HashMap<String, Object> mp)
+    {
+        if(mp.get("price").equals(0))
+        {
+            this.price = (int) mp.get("price");
+        }
+    }
+    
+}
