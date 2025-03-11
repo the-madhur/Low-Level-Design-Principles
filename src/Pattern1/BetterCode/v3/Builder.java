1c3e014156ae532af8929fec5cc83b6b8df4cefa
the-madhur <madhurhp245@gmail.com>
2025-03-11 16:20:47 +0530

    Better Code v3

---
+++ src/Pattern1/BetterCode/v3/Builder.java
@@ -0,0 +1,75 @@
+package Pattern1.BetterCode.v3;
+
+import java.util.List;
+
+public class Builder 
+{
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public int getPrice()
+    {
+        return this.price;
+    }
+
+    public String getDesc()
+    {
+        return this.desc;
+    }
+
+    // and so on..........some few getters.....  
+    
+    private String name;
+
+    private String desc;
+
+    private int price;
+
+    private String brand;
+    
+    private String category;
+
+    private int discount;
+
+    private String createdAt;
+
+    private String updatedAt;
+
+    private List<String> images;
+
+    // default Constructor
+    Builder()
+    {
+
+    }
+
+    // using Setters/Getters 
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setDesc(String desc)
+    {
+        this.desc = desc;
+    }
+
+    public void setPrice(int price)
+    {
+        if(price < 0) return;  // this validateion we do after creating objects in Main class that does not follow our Situation 2 - Validate the properties befor creating objects
+        this.price = price;
+    }
+
+    public void setBrand(String brand)
+    {
+        this.brand = brand;
+    }
+
+    public void setCategory(String category)
+    {
+        this.category = category;
+    }
+    
+}
