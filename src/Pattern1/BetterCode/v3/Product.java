75c0a03b47e2a954e18935ec1a0052a5b2b2259f
the-madhur <madhurhp245@gmail.com>
2025-03-11 16:20:43 +0530

    Better Code v3

---
+++ src/Pattern1/BetterCode/v3/Product.java
@@ -0,0 +1,34 @@
+package Pattern1.BetterCode.v3;
+
+import java.util.List;
+
+public class Product 
+{
+    private String name;
+
+    private String desc;
+
+    private int price;
+
+    private String brand;
+    
+    private String category;
+
+    private int discount;
+
+    private String createdAt;
+
+    private String updatedAt;
+
+    private List<String> images;
+
+    // default Constructor
+    Product(Builder b)
+    {
+        if(b.getPrice() > 0)
+        {
+            this.price = b.getPrice();
+        }
+    }
+    
+}
