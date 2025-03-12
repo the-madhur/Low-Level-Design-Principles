0d38272d303858cb34f91443e47f233a1ffb74ec
the-madhur <madhurhp245@gmail.com>
2025-03-12 16:27:32 +0530

    V4

---
+++ src/Pattern1/BetterCode/v4/Main.java
@@ -0,0 +1,15 @@
+package Pattern1.BetterCode.v4;
+
+public class Main 
+{
+    public static void main(String[] args) {
+        
+        Builder b = new Builder();
+        b.setBrand("Apple");
+        b.setName("Iphone 16");
+        b.setPrice(20000);
+
+        Product p = new Product(b);
+    }
+
+}
